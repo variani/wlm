c687d1b785eab29103ac8ddd91ee6888dbb20629
variani <machovariani@gmail.com>
2016-12-26 18:06:03 -0500

    new color

--- docs/R/02-figure-varcovar.R
+++ docs/R/02-figure-varcovar.R
@@ -1,11 +1,15 @@
 ### inc
 library(reshape)
 
+library(RColorBrewer)
 library(ggplot2)
 
 ### par
 N <- 12
 rho <- 0.5
+
+color <- "orange"
+color <- brewer.pal(5, "Set1")[2]
 
 ### matrices
 V1 <- diag(N)
@@ -16,15 +20,15 @@ V2 <- diag(val)
 V3 <- sapply(val, function(x) val * as.numeric(x == val))
 
 ### plot
-plot_mat <- function(mat, title = "") 
+plot_mat <- function(mat, title = "", color = "orange") 
 {
   pf <- melt(mat)
 
   p <- ggplot(pf, aes(X1, X2)) + geom_tile(aes(fill = value), color = "white") + 
-    scale_fill_gradient(low = "white", high = "purple")
+    scale_fill_gradient(low = "white", high = color)
   p <- p + scale_y_reverse()
   p <- p + guides(fill = "none") + theme_void() + theme(plot.title = element_text(hjust = 0.5))
-  
+
   p <- p + ggtitle(title)
   
   return(p)
@@ -33,9 +37,9 @@ plot_mat <- function(mat, title = "")
 png("varcovar-matrices-ggplot2.png", width = 3*480, height = 480)
 
 grid.arrange(
-  plot_mat(V1, title = "\nOrdinary Least Squares (OLS)\nR package stats"),
-  plot_mat(V2, title = "\nWeighted Least Squares (WLS)\nR package stats"),
-  plot_mat(V3, title = "\nGeneralized Least Squares (GLS)\nR package wls"),    
+  plot_mat(V1, title = "\nOrdinary Least Squares (OLS)\nR package stats", color = color),
+  plot_mat(V2, title = "\nWeighted Least Squares (WLS)\nR package stats", color = color),
+  plot_mat(V3, title = "\nGeneralized Least Squares (GLS)\nR package wls", color = color),    
   nrow = 1)
 
 dev.off()
