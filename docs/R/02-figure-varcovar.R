14c233454f547c5bd1ac5e6bac6e216e68248849
variani <machovariani@gmail.com>
2016-12-26 18:13:27 -0500

    enlarged font size

--- docs/R/02-figure-varcovar.R
+++ docs/R/02-figure-varcovar.R
@@ -3,6 +3,7 @@ library(reshape)
 
 library(RColorBrewer)
 library(ggplot2)
+library(gridExtra)
 
 ### par
 N <- 12
@@ -27,7 +28,8 @@ plot_mat <- function(mat, title = "", color = "orange")
   p <- ggplot(pf, aes(X1, X2)) + geom_tile(aes(fill = value), color = "white") + 
     scale_fill_gradient(low = "white", high = color)
   p <- p + scale_y_reverse()
-  p <- p + guides(fill = "none") + theme_void() + theme(plot.title = element_text(hjust = 0.5))
+  p <- p + guides(fill = "none") + theme_void() + 
+    theme(plot.title = element_text(hjust = 0.5, size = rel(2.5)))
 
   p <- p + ggtitle(title)
   
@@ -37,9 +39,9 @@ plot_mat <- function(mat, title = "", color = "orange")
 png("varcovar-matrices-ggplot2.png", width = 3*480, height = 480)
 
 grid.arrange(
-  plot_mat(V1, title = "\nOrdinary Least Squares (OLS)\nR package stats", color = color),
-  plot_mat(V2, title = "\nWeighted Least Squares (WLS)\nR package stats", color = color),
-  plot_mat(V3, title = "\nGeneralized Least Squares (GLS)\nR package wls", color = color),    
+  plot_mat(V1, title = "\nOrdinary Least Squares (OLS)", color = color),
+  plot_mat(V2, title = "\nWeighted Least Squares (WLS)", color = color),
+  plot_mat(V3, title = "\nGeneralized Least Squares (GLS)", color = color),    
   nrow = 1)
 
 dev.off()
